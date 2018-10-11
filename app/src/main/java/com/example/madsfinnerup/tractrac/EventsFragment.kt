f14fc5b38ebf12c0d1729a0c2a522ad644e033cc
Tardis <rina_bi@live.dk>
2018-10-11 13:28:31 +0200

    Created a java fragment for events

--- app/src/main/java/com/example/madsfinnerup/tractrac/EventsFragment.kt
+++ app/src/main/java/com/example/madsfinnerup/tractrac/EventsFragment.kt
@@ -7,13 +7,18 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import android.widget.ListView
 class EventsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false)
+
+        View view = inflater.inflate(R.layout.fragment_events, container, false)
+// Inflate the layout for this fragment
+
+        ListView events = (ListView) view.findViewById(R.id.eventlist);
+        return view;
     }
 
     override fun onAttach(context: Context?) {
