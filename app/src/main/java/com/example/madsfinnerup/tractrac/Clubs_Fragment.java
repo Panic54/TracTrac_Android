89ed64f5f0f6bc5af58fdbcd7cd1e3171c855d87
Iman Chelhi <iman_1997@live.dk>
2018-11-22 14:24:19 +0100

    Clubs fragment page set up

--- app/src/main/java/com/example/madsfinnerup/tractrac/Clubs_Fragment.java
+++ app/src/main/java/com/example/madsfinnerup/tractrac/Clubs_Fragment.java
@@ -1,14 +1,56 @@
 package com.example.madsfinnerup.tractrac;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class Clubs_Fragment extends Fragment {
+
+    int[] images = {R.drawable.french, R.drawable.swiss, R.drawable.ess, R.drawable.eurosail, R.drawable.redbull};
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_clubs, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View inflaterview = inflater.inflate(R.layout.fragment_clubs, container, false);
+
+        ListView clubs = inflaterview.findViewById(R.id.clubslist);
+        CustomAdapter clubsadapter = new CustomAdapter();
+        clubs.setAdapter(clubsadapter);
+        return inflaterview;
     }
+
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            view = getLayoutInflater().inflate(R.layout.custom_layout_clubs, null);
+            ImageView clubsimageview = view.findViewById(R.id.clubsimage);
+
+            clubsimageview.setImageResource(images[i]);
+            return view;
+        }
+    }
+
 }
